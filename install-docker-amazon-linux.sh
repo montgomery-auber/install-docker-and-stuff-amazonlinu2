0d08b38c747b8ca97bc2b5d0793217d66becf586
montgomery-auber <49875455+montgomery-auber@users.noreply.github.com>
2021-06-12 22:27:52 +0300

    install compose before reboot

--- install-docker-amazon-linux.sh
+++ install-docker-amazon-linux.sh
@@ -6,9 +6,8 @@ sudo usermod -a -G docker ec2-user
 sudo systemctl enable docker --now
 sudo chkconfig docker on
 sudo yum install -y git screen curl wget telnet nc iputils-ping
-sudo reboot
-
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo reboot
 
 # https://gist.github.com/npearce/6f3c7826c7499587f00957fee62f8ee9
 # https://aws.amazon.com/blogs/mt/keeping-ansible-effortless-with-aws-systems-manager/
